cf8b9292e54ca6efd8369caffe17167fdb8965dc
Abdiel <maganaabdiel02@gmail.com>
2024-10-10 23:33:04 -0600

    Update PRACT1 - Estruc Pilas.py

--- PRACT1 - Estruc Pilas.py
+++ PRACT1 - Estruc Pilas.py
@@ -18,28 +18,28 @@ class PilaPrac:
 
     def Insertar(self, elemento):
         if self.Pila_Llena():
-            print(f"No se puede insertar {elemento}: Pila llena.")
+            print(f"No se puede Insertar {elemento}: Pila llena.")
         else:
             self.Pila.append(elemento) 
             self.tope += 1  
-            print(f"Insertado: {elemento}. Pila ahora: {self.Pila}")
+            print(f"Insertando: {elemento}. Pila actual: {self.Pila}")
 
     def Eliminar(self):
         if self.Pila_Vacia():
-            print("No se puede eliminar: La pila ya está vacía.")
+            print("No se puede Eliminar: La pila ya está vacía.")
         else:
             elemento = self.Pila.pop()        
             self.tope -= 1
-            print(f"Eliminado: {elemento}. Pila ahora: {self.Pila}")
+            print(f"Eliminando: {elemento}. Pila actual: {self.Pila}")
 
 pila = PilaPrac(8)
 
-pila.insertar("X") 
-pila.insertar("Y") 
-pila.eliminar()
-pila.eliminar()    
-pila.eliminar()  
-pila.insertar("V")  
-pila.insertar("W") 
-pila.eliminar()    
-pila.insertar("R") 
+pila.Insertar("X") 
+pila.Insertar("Y") 
+pila.Eliminar()
+pila.Eliminar()    
+pila.Eliminar()  
+pila.Insertar("V")  
+pila.Insertar("W") 
+pila.Eliminar()    
+pila.Insertar("R") 
